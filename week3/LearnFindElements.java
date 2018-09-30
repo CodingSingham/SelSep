f8a1cb7af007ae1e8e969bbccad265e7facb6632
TestLeafPages <koushik.chatterjee@testleaf.com>
2018-09-30 13:33:35 +0530

    Add files via upload

---
+++ week3/LearnFindElements.java
@@ -0,0 +1,54 @@
+package week3;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class LearnFindElements {
+
+	public static void main(String[] args) {
+		//invoke chrome browser
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		//maximize window
+		driver.manage().window().maximize();
+		//load the URL
+		driver.get("http://www.crystalcruises.com/cruises/calendar?year=2018");
+		List<WebElement> allQuotes = driver.findElementsByLinkText("REQUEST A QUOTE");
+		int size = allQuotes.size();
+		System.out.println("The Number Of Matching q "+size);
+		/*WebElement firstOne = allQuotes.get(0);
+		firstOne.click();*/
+		WebElement lastOne = allQuotes.get(size-1);
+		lastOne.click();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
